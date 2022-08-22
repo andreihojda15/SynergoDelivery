037885c3cc06737d7e20f9898083d1511d85103d
andreihojda15 <andrei.hojda00@gmail.com>
2022-08-22 20:38:39 +0300

    - implemented drivers model and table

--- src/components/drivers/Drivers.js
+++ src/components/drivers/Drivers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from 'react-bootstrap'
+import { Table } from "react-bootstrap";
 
 /**
  * Driver model:
@@ -7,7 +7,7 @@ import { Table } from 'react-bootstrap'
  *  name
  *  phoneNumber
  *  carId // car guid, can be undefined => status: busy / available
- * 
+ *
  *  Table columns
  *  #
  *  Name
@@ -18,79 +18,68 @@ import { Table } from 'react-bootstrap'
 class Drivers extends React.Component {
   constructor(props) {
     super(props);
-    console.log(`constructor`);
 
     this.state = {
       drivers: [
         {
+          guid: "eb3aebe1-e3ee-4524-b87b-d62fd7f3ba71",
           name: "Mihai Popescu",
-          mobile: "0773546223",
-          email: "mihai.popescu@asdmail.com",
-          area: "zona Girocului",
-          car: "TM01ASD",
+          phoneNumber: "0773546223",
+          carId: "d1c51f3b-f13f-407f-b6e2-46c5bbc747ad",
         },
         {
+          guid: "813a7c01-ea18-4962-b152-71efcb5c2b05",
           name: "Jack Sparrow",
-          mobile: "0774387543",
-          email: "jack.sparrow@asdmail.com",
-          area: "zona Aradului",
-          car: "TM02ASD",
+          phoneNumber: "0774387543",
+          carId: "62ee4afa-b261-4f0a-9581-386cf9e0c4c0",
         },
         {
+          guid: "5f93d951-1dc6-4632-86dd-7b6b3fc51191",
           name: "Ion Ion",
-          mobile: "0773555876",
-          email: "ion.ion@asdmail.com",
-          area: "zona Soarelui",
-          car: "TM03ASD",
+          phoneNumber: "0773555876",
+          carId: "c1f0c515-4862-434c-91bb-b477504f5161",
         },
         {
+          guid: "7f71a066-74b6-43c8-bda3-8a6fe4fc1ed9",
           name: "Ilie Papadie",
-          mobile: "0773434666",
-          email: "ilie.papadie@asdmail.com",
-          area: "zona Brancoveanu",
-          car: "TM04ASD",
+          phoneNumber: "0773434666",
+          carId: "0e063045-c96a-4d34-8cc5-4ca2223616dc",
         },
         {
+          guid: "1dfdfb6e-8791-4766-8fe4-9973b965e1ac",
           name: "Tomy Motan",
-          mobile: "0773433985",
-          email: "tomy.motan@asdmail.com",
-          area: "Mosnita Noua",
-          car: "TM05ASD",
+          phoneNumber: "0773433985",
+          carId: "49ce9298-161a-4a04-82c0-31640f05dc31",
         },
         {
+          guid: "602ee2fa-e319-41aa-aac9-9ebad9d21c56",
           name: "Gigi Negru",
-          mobile: "0773322546",
-          email: "gigi.negru@asdmail.com",
-          area: "zona Sagului",
-          car: "TM06ASD"
+          phoneNumber: "0773322546",
+          carId: "be37bda1-0b19-43da-b874-ac359926737c",
         },
         {
+          guid: "fc80d8f4-d2c3-460d-8596-fb26d41df3d8",
           name: "Adrian Avram",
-          mobile: "0773775634",
-          email: "adrian.avram@asdmail.com",
-          area: "zona UMT",
-          car: "TM07ASD"
+          phoneNumber: "0773775634",
+          carId: "80575920-8b09-42be-90ae-e7000e9c0384",
         },
         {
+          guid: "0bc15d56-f542-4228-802b-6ed877b87474",
           name: "Dan Georgescu",
-          mobile: "0773999444",
-          email: "mihai.popescu@asdmail.com",
-          area: "Dumbravita",
-          car: "TM08ASD",
+          phoneNumber: "0773999444",
+          carId: "bad6d196-a0f1-47bb-8e95-69ace085bcee",
         },
         {
+          guid: "6a0ee78b-f782-47e7-b178-af373cfa159b",
           name: "Octavian Marinescu",
-          mobile: "0773664488",
-          email: "octavian.marinescu@asdmail.com",
-          area: "zona Centru",
-          car: "TM09ASD",
+          phoneNumber: "0773664488",
+          carId: undefined,
         },
         {
+          guid: "329568b3-e130-4515-95ef-91cba734f52c",
           name: "Grigore Savu",
-          mobile: "0773338644",
-          email: "grigore.savu@asdmail.com",
-          area: "Ghiroda",
-          car: "TM10ASD",
+          phoneNumber: "0773338644",
+          carId: undefined,
         },
       ],
     };
@@ -98,32 +87,26 @@ class Drivers extends React.Component {
 
   render() {
     return (
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Mobile</th>
-              <th>Email</th>
-              <th>Area of delivery</th>
-              <th>Car number</th>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Phone Number</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.drivers.map((item, i) => (
+            <tr key={item.guid}>
+              <td>{i + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.phoneNumber}</td>
+              <td>{item.carId ? "Available" : "Busy"}</td>
             </tr>
-            </thead>
-            <tbody>
-            { 
-              this.state.drivers.map((item, i) => 
-                <tr key={i}>
-                  <td>{i+1}</td>
-                  <td>{item.name}</td>
-                  <td>{item.mobile}</td>
-                  <td>{item.email}</td>
-                  <td>{item.area}</td>
-                  <td>{item.car}</td>
-                </tr>
-              )
-            }
-          </tbody>
-        </Table>
+          ))}
+        </tbody>
+      </Table>
     );
   }
 }

3512c5400ab6abb64f4b6038c94794739cbd9a80
andreihojda15 <andrei.hojda00@gmail.com>
2022-08-23 11:46:27 +0300

    - added Links to navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import './style/bootstrap.scss';
+import "./style/bootstrap.scss";
 
-import Header from './components/layout/Header';
-import Content from './components/layout/Content';
-import Footer from './components/layout/Footer';
+import Header from "./components/layout/Header";
+import Content from "./components/layout/Content";
+import Footer from "./components/layout/Footer";
 
-import Packages from './components/packages/Package';
-import Cars from './components/cars/Cars';
-import Drivers from './components/drivers/Drivers';
+import Packages from "./components/packages/Package";
+import Cars from "./components/cars/Cars";
+import Drivers from "./components/drivers/Drivers";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header>
+          <Routes>
+            <Route path="/" element={<Packages />}>
+              <Route path="/cars" element={<Cars />} />
+              <Route path="/drivers" element={<Drivers />} />
+            </Route>
+          </Routes>
+        </Header>
+      </BrowserRouter>
       <Content>
-        <Packages></Packages>
+        {/* <Packages></Packages>
         <Cars></Cars>
-        <Drivers></Drivers>
+        <Drivers></Drivers> */}
       </Content>
       <Footer />
     </div>

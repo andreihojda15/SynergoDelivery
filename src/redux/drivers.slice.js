d3b9e7a48e1fb2ab07948621324af3e377bdd39a
Izabela <izabela.hent@gmail.com>
2022-08-25 09:22:03 +0300

    drivers component - link to redux store

---
+++ src/redux/drivers.slice.js
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import DriversService from '../services/drivers.service'
+
+export const getDrivers = createAsyncThunk(
+    'getDrivers',
+    async () => {
+        try {
+            const response = await DriversService.getDrivers()
+            // TODO: add proper handling for a fail
+            console.log(`--- successful response: ${JSON.stringify(response)}`)
+            return response
+        } catch (err) {
+            console.log(`--- error response: ${JSON.stringify(err)}`)
+            return err
+        }
+    }
+)
+
+// Then, handle actions in your reducers:
+const driversSlice = createSlice({
+    name: 'drivers',
+    initialState: {
+        isLoading: false,
+        drivers: [],
+    },
+    reducers: {
+        // standard reducer logic, with auto-generated action types per reducer
+    },
+    extraReducers: (builder) => {
+        // Add reducers for additional action types here, and handle loading state as needed
+        builder.addCase(getDrivers.pending, (state, action) => {
+            console.log('--- get drivers pending...')
+            state.isLoading = true
+            state.drivers = []
+        })
+
+        builder.addCase(getDrivers.fulfilled, (state, action) => {
+            console.log('--- get drivers fulfilled...')
+            state.isLoading = false
+            state.drivers = action.payload || []
+        })
+
+        builder.addCase(getDrivers.rejected, (state, action) => {
+            console.log('--- get drivers rejected...')
+            state.isLoading = false
+            state.drivers = []
+            // TODO: handle error messages
+        })
+    },
+})
+
+export default driversSlice.reducer

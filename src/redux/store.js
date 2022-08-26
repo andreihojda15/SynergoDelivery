ece64868518d0164d41e46a14d3c60440905d0be
Vlad Balan <vladbalan002@yahoo.com>
2022-08-26 09:52:26 +0300

    -cars redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import packagesReducer from './packages.slice'
 
+import carsReducer from './cars.slice'
+
+
 export default configureStore({
   reducer: {
     packages: packagesReducer,
+    cars: carsReducer,
+
   },
 })
